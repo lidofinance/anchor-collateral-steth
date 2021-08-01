3b0c2414c6397b58c999d887cdd4710c1e108ae0
Sam Kozin <sam.kozin@gmail.com>
2021-08-01 23:48:12 +0300

    test: add a script for running the final post-deploy checks

---
+++ utils/log.py
@@ -0,0 +1,17 @@
+color_hl = '\x1b[38;5;141m'
+color_green = '\033[92m'
+color_gray = '\x1b[0;m'
+color_end = '\033[0m'
+
+
+def highlight(text, color = color_hl):
+    return f'{color}{text}{color_end}'
+
+
+def ok(text, value = None):
+    result = highlight('[ok] ', color_green) + text
+
+    if value is not None:
+        result += ': ' + highlight(value, color_hl)
+
+    print(result)
