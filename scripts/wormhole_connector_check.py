e9cb8fc7565a62f6bc3773fa2b235c504244515a
Gleb Ershov <hostgame@list.ru>
2021-10-27 22:43:46 +0600

    Submitting stETH through wormhole token bridge in wormhole_connector_check

--- scripts/wormhole_connector_check.py
+++ scripts/wormhole_connector_check.py
@@ -10,6 +10,7 @@ from brownie import (
 import utils.log as log
 from utils.mainnet_fork import chain_snapshot
 
+TERRA_ADDRESS = '0xabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd'
 
 def assert_equals(desc, actual, expected):
     assert actual == expected
@@ -26,7 +27,8 @@ def main():
     ust_token = interface.ERC20('0xa47c8bf37f92aBed4A126BDA807A7b7498661acD')
     steth_token = interface.Lido('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84')
     bridge_connector_shuttle = BridgeConnectorShuttle.at('0x513251faB2542532753972B8FE9A7b60621affaD')
-    token_bridge_wormhole = interface.Bridge('0x736D2A394f7810C17b3c6fEd017d5BC7D60c077d')
+    token_bridge_wormhole = interface.Bridge('0x3ee18B2214AFF97000D974cf647E7C347E8fa585')
+    wormhole = interface.Wormhole('0x98f3c9e6E3fAce36bAAd05FE09d375Ef1464288B')
 
     log.ok('dev multisig', dev_multisig)
 
@@ -62,4 +64,41 @@ def main():
 
     log.ok('AnchorVault', vault.address)
     assert_equals('  bridge_connector', vault.bridge_connector(), bridge_connector_wormhole.address)
-    # TODO: By some reason same is not applied to vault_impl. It such behavior correct?
+
+    with chain_snapshot():
+        dev_multisig_acct = accounts.at(dev_multisig, force=True)
+        liquidations_admin = accounts.at('0x1A9967A7b0c3dd39962296E53F5cf56471385dF2', force=True)
+
+        print()
+
+        print('Submitting stETH...')
+        beth_supply = beth_token.totalSupply()
+
+        holder_1 = accounts[0]
+
+        steth_token.submit(ZERO_ADDRESS, {'from': holder_1, 'value': 3 * 10**18})
+        assert steth_token.balanceOf(holder_1) > 0
+        assert beth_token.balanceOf(token_bridge_wormhole) == 0
+        steth_token.approve(vault, 2 * 10**18, {'from': holder_1})
+
+        tx = vault.submit(2 * 10**18, TERRA_ADDRESS, b'', {'from': holder_1})
+        tx.info()
+
+        bridge_balance = beth_token.balanceOf(token_bridge_wormhole)
+
+        assert bridge_balance == 2 * 10**18
+        assert beth_token.balanceOf(holder_1) == 0
+        assert beth_token.totalSupply() == beth_supply + 2 * 10**18
+
+        assert 'LogMessagePublished' in tx.events
+        assert tx.events['LogMessagePublished']['payload'] == '0x01000000000000000000000000000000000000000000000000000000000bebc200000000000000000000000000707f9118e33a9b8998bea41dd0d46f38bb963fc80002abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd00030000000000000000000000000000000000000000000000000000000000000000'
+
+        log.ok('bridge bETH balance', bridge_balance / 10**18)
+
+        print()
+        
+        print('Selling rewards...')
+
+        tx = vault.collect_rewards({'from': liquidations_admin})
+        tx.info()
+        # @TODO: get to collect_rewards() stage when it calls forward_ust()
