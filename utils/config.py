60b6d112ab5e22c047e889fd279ea75c1a740173
Sam Kozin <sam.kozin@gmail.com>
2022-01-22 13:47:22 +0300

    deploy: add deployed migration components' addresses

--- utils/config.py
+++ utils/config.py
@@ -14,8 +14,8 @@ wormhole_token_bridge_addr = '0x3ee18B2214AFF97000D974cf647E7C347E8fa585'
 vault_proxy_addr = '0xA2F987A546D4CD1c607Ee8141276876C26b72Bdf'
 vault_impl_addr = '0x0627054d17eAe63ec23C6d8b07d8Db7A66ffd45a'
 beth_token_addr = '0x707F9118e33A9B8998beA41dd0d46f38bb963FC8'
-bridge_connector_addr = '0x513251faB2542532753972B8FE9A7b60621affaD'
-rewards_liquidator_addr = '0x082a5956D63b44685a7CCA89379D565C439fdf3C'
+bridge_connector_addr = '0x2618e94a7F6118acED2E51e0a05da43D2e2eD40C'
+rewards_liquidator_addr = '0xE3c8A4De3b8A484ff890a38d6D7B5D278d697Fb7'
 insurance_connector_addr = '0x2BDfD3De0fF23373B621CDAD0aD3dF1580efE701'
 dev_multisig_addr = '0x3cd9F71F80AB08ea5a7Dca348B5e94BC595f26A0'
 vault_liquidations_admin_addr = '0x1A9967A7b0c3dd39962296E53F5cf56471385dF2'
@@ -30,10 +30,9 @@ rew_liq_max_usdc_ust_price_difference_percent = 3
 rew_liq_max_steth_ust_price_difference_percent = 5
 
 # Wormhole migration addresses
-wh_migrator_addr = None
-wh_migration_executor_addr = None
+wh_migrator_addr = '0x02454e9dd8D8d49d4A20Df27F5E1c3a51b4c1C27'
+wh_migration_executor_addr = '0xe19fc582dd93FA876CF4061Eb5456F310144F57b'
 
-gas_price = "90 gwei"
 
 def get_is_live():
     return network.show_active() != "development"
