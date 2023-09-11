f839b5ee9a285cc3466fda2b60aa8196d7f0ab02
Logachev Nikita <logach3@gmail.com>
2023-09-11 13:01:15 +0300

    add source publishing

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -16,8 +16,10 @@ def set_console_globals(**kwargs):
 
 def main():
     deployer = config.get_deployer_account(config.get_is_live())
+    publish_source = config.get_is_live()
 
-    print("Deployer:", deployer)
+    print("Deployer\t", deployer)
+    print("Publish source\t", publish_source)
 
     sys.stdout.write("Proceed? [y/n]: ")
     if not config.prompt_bool():
@@ -29,17 +31,13 @@ def main():
     if config.get_is_live():
         tx_params["priority_fee"] = "2 gwei"
 
-    anchor_new_vault = deploy(tx_params)
+    anchor_new_vault = deploy(tx_params, publish_source)
 
     print("Vault address\t", anchor_new_vault)
     print("Vault petrified\t", anchor_new_vault.version() == 2**256-1)
 
-def deploy(tx_params):
-    vault = AnchorVault.deploy(tx_params)
+
+def deploy(tx_params, publish_source=False):
+    vault = AnchorVault.deploy(tx_params, publish_source=publish_source)
     vault.petrify_impl(tx_params)
     return vault
-
-
-
-
-
